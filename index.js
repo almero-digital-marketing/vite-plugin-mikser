2688ffd15236c2b060375712b2b16d215f1246c0
dimitarkolev <dk@almero.bg>
2023-01-04 17:10:29 +0000

    Fix

--- index.js
+++ index.js
@@ -3,38 +3,44 @@ import { JSONFileSync } from 'lowdb/node'
 import path from 'node:path'
 
 const mikserPlugin = ({ outputFolder = 'out', runtimeFolder = 'runtime' } = {}) => {
-    const adapter = new JSONFileSync(path.join(runtimeFolder, 'database.json'))
-    const database = new LowSync(adapter)
-    database.read()
-    database.data ||= { results: [] }
-
-    const input = {}
-
-    database.data.results
-    .filter(entity => entity.destination && entity.destination.indexOf('.html') > -1)
-    .forEach(entity => {
-        input[entity.destination.substring(1)] = entity.destination
-    })
-
     return {
         name: 'mikser',
-        config: () => ({
-            root: outputFolder,
-            appType: 'mpa',
-            clearScreen: false,
-            server: {
-                host: true
-            },
-            build: {
-                outDir: '../dist',
-                assetsDir: 'bundle',
-                emptyOutDir: true,
-                copyPublicDir: false,
-                rollupOptions: {
-                    input
+        config: (config, { command }) => {
+            const mikserConfig = {
+                root: outputFolder,
+                appType: 'mpa',
+                clearScreen: false,
+                server: {
+                    host: true
+                },
+                build: {
+                    outDir: '../dist',
+                    assetsDir: 'bundle',
+                    emptyOutDir: true,
+                    copyPublicDir: false,
+                    ssr: true,
+                    rollupOptions: {
+                    }
                 }
             }
-        }),
+            
+            if (command === 'build') {
+                const adapter = new JSONFileSync(path.join(runtimeFolder, 'database.json'))
+                const database = new LowSync(adapter)
+                database.read()
+                database.data ||= { results: [] }
+
+                const input = {}
+                database.data.results
+                .filter(entity => entity.destination && entity.destination.indexOf('.html') > -1)
+                .forEach(entity => {
+                    input[entity.destination.substring(1)] = entity.destination
+                })
+                mikserConfig.build.rollupOptions.input = input
+            }
+
+            return mikserConfig
+        },
     }
 }
 export default mikserPlugin
