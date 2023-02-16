8179289c69ca8378123b097f85d83b86c4923052
dimitarkolev <dk@almero.bg>
2023-02-16 17:03:10 +0000

    Better mikser integration

--- index.js
+++ index.js
@@ -1,48 +1,45 @@
+import path from 'node:path'
 import { LowSync } from 'lowdb'
 import { JSONFileSync } from 'lowdb/node'
-import path from 'node:path'
-import { rmSync } from 'node:fs'
 
-const mikserPlugin = ({ outputFolder = 'out', runtimeFolder = 'runtime' } = {}) => {
+const mikserPlugin = ({ outputFolder, distFolder, buildFolder, runtimeFolder } = {}) => {
     return {
         name: 'mikser',
-        config: (config, { command }) => {
+        config: (config, { command, mode }) => {
+            const root = command == 'build' ? buildFolder : process.cwd()
+            const publicDir = command == 'serve' ? path.join(outputFolder, 'public') : 'public'
+
             Object.assign(config, {
-                root: outputFolder,
+                root,
                 appType: 'mpa',
-                clearScreen: false,
-                server: {
-                    host: true
-                },
-                build: {
-                    outDir: '../dist',
-                    assetsDir: 'bundle',
-                    emptyOutDir: false,
-                }
+                publicDir,
             })
 
-            try {
-                rmSync('dist/bundle', { recursive: true })
-            } catch (err) {
-                if (err.code != 'ENOENT')
-                throw err
-            }
-            
-            if (command === 'build') {
-                const adapter = new JSONFileSync(path.join(runtimeFolder, 'database.json'))
-                const database = new LowSync(adapter)
-                database.read()
-                database.data ||= { results: [] }
+            const adapter = new JSONFileSync(path.join(runtimeFolder, `database.${mode}.json`))
+            const database = new LowSync(adapter)
+            database.read()
+            database.data ||= { results: [] }
 
-                const input = {}
-                database.data.results
-                .filter(entity => entity.destination && entity.destination.indexOf('.html') > -1)
-                .forEach(entity => {
-                    input[entity.destination.substring(1)] = entity.destination
-                })
-                config.build.rollupOptions ||= {}
-                config.build.rollupOptions.input = input
-            }
+            const input = {}
+            database.data.results
+            .filter(entity => entity.destination && entity.destination.indexOf('.html') > -1)
+            .forEach(entity => {
+                input[entity.destination.substring(1)] = entity.destination
+            })
+
+            config.build ||= {}
+            Object.assign(config.build, {
+                outDir: distFolder,
+                assetsDir: 'bundle',
+                emptyOutDir: true,
+                copyPublicDir: true,
+                sourcemap: mode == 'development',
+            })
+
+            config.build.rollupOptions ||= {}
+            Object.assign(config.build.rollupOptions, {
+                input
+            })
         },
     }
 }
